d209e547786b6b6487860f9fcde64b0e87574787
johnkdunyo <johnkdunyo@gmail.com>
2024-01-09 14:42:15 +0000

    added lamdba function to handle all blog requests, and connected it to the apigatway resouce in the app stack

--- lib/serverless_crud_api_stack.py
+++ lib/serverless_crud_api_stack.py
@@ -1,9 +1,11 @@
+import os
 from aws_cdk import (
     # Duration,
     core,
     Stack,
     aws_dynamodb as dynamodb,
     aws_apigateway as apigateway,
+    aws_lambda as lambda_,
     # aws_sqs as sqs,
 )
 from constructs import Construct
@@ -18,7 +20,9 @@ class ServerlessCrudApiStack(Stack):
         # Define the DynamoDB table
         # blog: PK = blogID, SK = dateCreated, otherFields = body, ImageURL
         blog_table = dynamodb.Table(
-            self,"blogTable",
+            self,
+            id="blogTable",
+            table_name="Blog Table",
             partition_key=dynamodb.Attribute(
                 name="blogId",
                 type=dynamodb.AttributeType.STRING
@@ -31,13 +35,33 @@ class ServerlessCrudApiStack(Stack):
             removal_policy=core.RemovalPolicy.DESTROY  # Change to RETAIN if you want to keep the table on stack deletion
         )
 
-
-        # Create the API Gateway
-        apigw = apigateway.RestApi(self, 'blogRestAPI',
-            rest_api_name='Blog API',
-            default_cors_preflight_options={
-                'allow_origins': apigateway.Cors.ALL_ORIGINS,
-                'allow_methods': apigateway.Cors.ALL_METHODS
+        # lamdba handle for the blogs
+        blogs_handler_function = lambda_.Function(
+            self, 'ProductLambdaFunction',
+            runtime=lambda_.Runtime.NODEJS_14_X,
+            handler='index.handler',
+            code=lambda_.Code.from_asset(os.path.join(os.path.dirname(__file__), '/../resources/blog')), #path to lamdba functions
+            environment={
+                'PRIMARY_KEY': 'id',
+                'DYNAMODB_TABLE_NAME': blog_table.table_name
             },
+            bundling={
+                'externalModules': ['aws-sdk']
+            }
         )
+
+       
+        # Create the API Gateway
+        apigw = apigateway.LambdaRestApi(
+            self, 
+            rest_api_name="Blog RestAPI",
+            handler=blogs_handler_function,
+            proxy=False
+        )
+
+
+        
+
+
+
 

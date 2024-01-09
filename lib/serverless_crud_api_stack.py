cb057d94a36e7e3dc7a1dd84a18d6bd45e84058b
johnkdunyo <johnkdunyo@gmail.com>
2024-01-09 10:48:10 +0000

    added dynamodb resource to stack

--- lib/serverless_crud_api_stack.py
+++ lib/serverless_crud_api_stack.py
@@ -14,4 +14,19 @@ class ServerlessCrudApiStack(Stack):
 
         # The code that defines your stack goes here
 
+        # Define the DynamoDB table
+        # blog: PK = blogID, SK = dateCreated, otherFields = body, ImageURL
+        blog_table = dynamodb.Table(
+            self,"blogTable",
+            partition_key=dynamodb.Attribute(
+                name="blogId",
+                type=dynamodb.AttributeType.STRING
+            ),
+            sort_key=dynamodb.Attribute(
+                name="dateCreated",
+                type=dynamodb.AttributeType.STRING
+            ),
+            billing_mode=dynamodb.BillingMode.PAY_PER_REQUEST,  # or PROVISIONED based on your needs
+            removal_policy=core.RemovalPolicy.DESTROY  # Change to RETAIN if you want to keep the table on stack deletion
+        )
 
